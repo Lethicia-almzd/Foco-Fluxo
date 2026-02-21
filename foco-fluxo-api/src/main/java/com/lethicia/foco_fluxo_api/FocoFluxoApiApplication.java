788fa44bf34858c87594a4c8844c492c5c245c78
Lethicia Almeida <lethicia.almeida62@gmail.com>
2026-02-20 23:54:09 -0300

    chore: estrutura inicial do projeto com Spring Boot

---
+++ foco-fluxo-api/src/main/java/com/lethicia/foco_fluxo_api/FocoFluxoApiApplication.java
@@ -0,0 +1,13 @@
+package com.lethicia.foco_fluxo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocoFluxoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocoFluxoApiApplication.class, args);
+	}
+
+}
---
+++ foco-fluxo-api/src/test/java/com/lethicia/foco_fluxo_api/FocoFluxoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lethicia.foco_fluxo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocoFluxoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
